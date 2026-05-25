a946ec8d5b4bf7d3e189c0d70bef043ada692c76
Arise Damilare <arisedamilare5@gmail.com>
2026-05-25 12:05:54 +0100

    Bank operations with modularisation

---
+++ config.js
@@ -0,0 +1,118 @@
+// MVC  - model, views and controllers
+export default class Bank{
+    #database = []
+
+    constructor(){
+        this.#database = JSON.parse(localStorage.getItem('users')) || []
+    }
+
+
+    create_account(fullname, email, password){
+        if(fullname=="" || email=="" || password==""){
+            return "Input fields are required"
+        }
+
+        let check_email = this.#database.find( user => user.email === email )
+        // console.log(check_email);
+        
+        if(check_email){
+            return 'Email already exist'
+        }
+
+        let user = {
+            fullname,
+            email,
+            password,
+            balance: 0
+        }
+
+        this.#database.push(user)
+        // [{}]
+        // localStorage.setItem('users', JSON.stringify(this.#database))
+        localStorage.users = JSON.stringify(this.#database)
+        return "Registration successfull"
+    }
+
+    login(email, password){
+        if(email==null || password==null){
+            return "Input fields are required"
+        }
+
+        let user = this.#database.find( user => user.email === email && user.password === password )
+        if(user){
+            localStorage.loggedEmail = user.email
+            return {
+                status: true,
+                message: "Login successful"
+            }
+        }else{
+            return {
+                status: false,
+                message: "Incorrect email or password"
+            }
+        }
+        
+    }
+
+    getUser(email){
+        let user = this.#database.find( user => user.email === email )
+        return user
+    }
+
+    getBalance(email){
+        let user = this.getUser(email)
+        if(user){
+            return `$${user.balance}`
+        }
+        else{
+            return null
+        }
+    }
+
+    withdraw(email, amount){
+        let user = this.getUser(email)
+        if(!user){
+            return "User not found"
+        }
+
+        if(amount < 1){
+            return 'Invalid amount'
+        }
+        else if(amount > user.balance){
+            return 'Insufficient fund'
+        }
+        else{
+            user.balance -= amount
+            // update localstorage
+            localStorage.users = JSON.stringify(this.#database)
+            
+            return 'Transaction successfull'
+        }
+    }
+
+    deposit(email, amount){
+        if(amount < 1){
+            return 'Invalid amount'
+        }
+
+        let user = this.getUser(email)
+        
+        if(user){
+            user.balance += amount
+            console.log(user);
+
+            // update localstorage
+            localStorage.users = JSON.stringify(this.#database)
+            
+        }
+
+        // this.#balance += amount
+        return 'Transaction successful'
+    }
+
+}
+
+
+// export class Transaction{
+
+// }
